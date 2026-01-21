5ce6f2b87f3ae956b57edcbb5cc40be77a446085
Konstantinos Papoulias <kostaspapoulias@gmail.com>
2026-01-21 20:15:39 +0200

    face recognition fixed forgotten

--- frontend/src/app/pages/tasks/tasks.module.ts
+++ frontend/src/app/pages/tasks/tasks.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksViewComponent } from './tasks-view/tasks-view.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TasksComponent } from './tasks.component';
 
 
 @NgModule({
   declarations: [
+    TasksComponent,
     TasksViewComponent
   ],
   imports: [
